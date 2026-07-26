ea80ce2129ee202aaccae3c302abf815a09a9347
barnesjayren0-sudo <barnesjayren0@gmail.com>
2026-07-26 12:52:37 +0800

    v1.0 - Finalized main client class

--- src/main/java/com/jay/hackclient/JayHackClient.java
+++ src/main/java/com/jay/hackclient/JayHackClient.java
@@ -5,12 +5,16 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 import com.jay.hackclient.module.ModuleManager;
 import com.jay.hackclient.module.modules.*;
 
 public class JayHackClient implements ClientModInitializer {
+
+    public static final String NAME = "Jay's Hack Client";
+    public static final String VERSION = "1.0.0";
 
     public static JayHackClient INSTANCE;
     public static ModuleManager moduleManager;
@@ -22,7 +26,7 @@ public class JayHackClient implements ClientModInitializer {
         INSTANCE = this;
         moduleManager = new ModuleManager();
 
-        // === Sword PvP Modules ===
+        // Register all Sword PvP modules
         moduleManager.register(new KillAura());
         moduleManager.register(new AutoSword());
         moduleManager.register(new Reach());
@@ -32,7 +36,7 @@ public class JayHackClient implements ClientModInitializer {
         moduleManager.register(new ESP());
         moduleManager.register(new NoSlow());
 
-        // ClickGUI key (Right Shift)
+        // Right Shift = ClickGUI / Info
         clickGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.jayhackclient.clickgui",
                 InputUtil.Type.KEYSYM,
@@ -42,13 +46,17 @@ public class JayHackClient implements ClientModInitializer {
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (clickGuiKey.wasPressed() && client.player != null) {
-                client.player.sendMessage(net.minecraft.text.Text.literal("§b[Jay's Hack Client] §fClickGUI coming soon - Modules loaded!"), false);
+                client.player.sendMessage(Text.literal("§7[§bJay§7] §f" + NAME + " v" + VERSION + " - Sword PvP"), false);
+                client.player.sendMessage(Text.literal("§7Modules loaded: §a" + moduleManager.getModules().size()), false);
             }
 
             moduleManager.onTick();
         });
 
-        System.out.println("[Jay's Hack Client] v1.0 - Sword PvP Focused for 1.21.11");
-        System.out.println("[Jay's Hack Client] Modules: KillAura, AutoSword, Reach, Criticals, AutoSprint, Velocity, ESP, NoSlow");
+        System.out.println("========================================");
+        System.out.println("[" + NAME + "] v" + VERSION + " loaded");
+        System.out.println("[" + NAME + "] Sword PvP Focused Client");
+        System.out.println("[" + NAME + "] Modules: " + moduleManager.getModules().size());
+        System.out.println("========================================");
     }
 }
