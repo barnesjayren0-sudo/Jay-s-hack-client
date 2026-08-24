fa169e3a5a18f49a0ccf3977eb82046efd0233cb
barnesjayren0-sudo <barnesjayren0@gmail.com>
2026-08-24 19:48:42 +0800

    v1.19.1 — VERSION bump

--- src/main/java/com/jay/hackclient/JayHackClient.java
+++ src/main/java/com/jay/hackclient/JayHackClient.java
@@ -29,7 +29,7 @@ import com.jay.hackclient.settings.ClientSettings;
 public class JayHackClient implements ClientModInitializer {
 
     public static final String NAME = "Jay's Hack Client";
-    public static final String VERSION = "1.19.0";
+    public static final String VERSION = "1.19.1";
 
     public static JayHackClient INSTANCE;
     public static ModuleManager moduleManager;
