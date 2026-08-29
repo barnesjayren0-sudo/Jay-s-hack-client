049fd5d715e2441169c675f856e1e68907a8d8c3
barnesjayren0-sudo <barnesjayren0@gmail.com>
2026-08-29 19:34:05 +0800

    Bump VERSION to 1.36.0

--- src/main/java/com/jay/hackclient/JayHackClient.java
+++ src/main/java/com/jay/hackclient/JayHackClient.java
@@ -30,7 +30,7 @@ import com.jay.hackclient.settings.ClientSettings;
 public class JayHackClient implements ClientModInitializer {
 
     public static final String NAME = "JAY CLIENT";
-    public static final String VERSION = "1.35.0";
+    public static final String VERSION = "1.36.0";
 
     public static JayHackClient INSTANCE;
     public static ModuleManager moduleManager;
