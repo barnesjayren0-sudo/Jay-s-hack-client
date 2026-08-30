9e9aa73ee2f418acb9b68ec674dedeb2e5ebb401
barnesjayren0-sudo <barnesjayren0@gmail.com>
2026-08-30 19:08:34 +0800

    VERSION 1.42.1 polish tag

--- src/main/java/com/jay/hackclient/JayHackClient.java
+++ src/main/java/com/jay/hackclient/JayHackClient.java
@@ -30,7 +30,7 @@ import com.jay.hackclient.settings.ClientSettings;
 public class JayHackClient implements ClientModInitializer {
 
     public static final String NAME = "JAY CLIENT";
-    public static final String VERSION = "1.42.0";
+    public static final String VERSION = "1.42.1";
 
     public static JayHackClient INSTANCE;
     public static ModuleManager moduleManager;
