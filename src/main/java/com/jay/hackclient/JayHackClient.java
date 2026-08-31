c0facdb41f400f99efa3a82c891ef331f397fd26
barnesjayren0-sudo <barnesjayren0@gmail.com>
2026-08-31 12:50:37 +0800

    JayHackClient VERSION 1.45.0

--- src/main/java/com/jay/hackclient/JayHackClient.java
+++ src/main/java/com/jay/hackclient/JayHackClient.java
@@ -31,7 +31,7 @@ import com.jay.hackclient.settings.ClientSettings;
 public class JayHackClient implements ClientModInitializer {
 
     public static final String NAME = "JAY CLIENT";
-    public static final String VERSION = "1.44.0";
+    public static final String VERSION = "1.45.0";
 
     public static JayHackClient INSTANCE;
     public static ModuleManager moduleManager;
