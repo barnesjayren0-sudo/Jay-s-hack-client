0e830f724ac7ee63ccb202b7518a6981ff85caf7
barnesjayren0-sudo <barnesjayren0@gmail.com>
2026-08-31 09:19:26 +0800

    Fix CameraMixin: Camera.update uses World not BlockView (1.21.11 crash)

--- src/main/java/com/jay/hackclient/mixin/CameraMixin.java
+++ src/main/java/com/jay/hackclient/mixin/CameraMixin.java
@@ -3,13 +3,16 @@ package com.jay.hackclient.mixin;
 import com.jay.hackclient.module.modules.Freecam;
 import net.minecraft.client.render.Camera;
 import net.minecraft.entity.Entity;
-import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+/**
+ * 1.21.11: Camera.update first arg is World (class_1937), not BlockView (class_1922).
+ */
 @Mixin(Camera.class)
 public abstract class CameraMixin {
 
@@ -20,7 +23,7 @@ public abstract class CameraMixin {
     protected abstract void setPos(double x, double y, double z);
 
     @Inject(method = "update", at = @At("TAIL"))
-    private void jay$applyFreecam(BlockView area, Entity focusedEntity, boolean thirdPerson,
+    private void jay$applyFreecam(World area, Entity focusedEntity, boolean thirdPerson,
                                   boolean inverseView, float tickDelta, CallbackInfo ci) {
         if (!Freecam.active) return;
         setPos(Freecam.x, Freecam.y, Freecam.z);
